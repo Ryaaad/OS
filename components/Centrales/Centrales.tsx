904defb1e2b242f5e992eb02cb11dc9760f4187a
Ryaaad <87044522+Ryaaad@users.noreply.github.com>
2023-06-04 01:35:43 +0100

    Centrales Filtering

--- components/Centrales/Centrales.tsx
+++ components/Centrales/Centrales.tsx
@@ -17,15 +17,77 @@ interface props{
 const Ctrl:React.FC<props> = (props) => {
  
   
-   const [DeleteClick, setDeleteClick] = useState(false)
-   const [Type, setType] = useState<any>()
-    const [firstFilter, setfirstFilter] = useState(true)
-    const [secondFilter, setsecondFilter] = useState(true)
-    const [inputValue, setInputValue] = useState<any>();
-   
-    const [FiltedCentrales, setFiltedCentrales] = useState<any>()
-    const [centraleData, setcentraleData] = useState<any>([0])
-   
+  const [DeleteClick, setDeleteClick] = useState(false)
+  const [Type, setType] = useState<any>()
+  const [firstFilter, setfirstFilter] = useState(true)
+  const [secondFilter, setsecondFilter] = useState(true)
+  const [inputValue, setInputValue] = useState<any>();
+  const [ChosenFiltrage, setChosenFiltrage] = useState<undefined | String>()
+  const [Filtring, setFiltring] = useState<number | String>(0)
+  const [FiltedCentrales, setFiltedCentrales] = useState<any>()
+  const [centraleData, setcentraleData] = useState<any>([0])
+  const Wilayas=[
+    {value:0,name:"All"},
+    {value:1,name:"Adrar"},
+    {value:2,name:"Chlef"},
+    {value:3,name:"Laghouat"},
+    {value:4,name:"Oum El Bouaghi"},
+    {value:5,name:"Batna"},
+    {value:6,name:"Béjaïa"},
+    {value:7,name:"Biskra"},
+    {value:8,name:"Béchar"},
+    {value:9,name:"Blida"},
+    {value:10,name:"Bouira"},
+    {value:11,name:"Tamanrasset"},
+    {value:12,name:"Tébessa"},
+    {value:13,name:"Tlemcen"},
+    {value:14,name:"Tiaret"},
+    {value:15,name:"Tizi Ouzou"},
+    {value:16,name:"Alger"},
+    {value:17,name:"Djelfa"},
+    {value:18,name:"Jijel"},
+    {value:19,name:"Sétif"},
+    {value:20,name:"Saïda"},
+    {value:21,name:"Skikda"},
+    {value:22,name:"Sidi Bel Abbès"},
+    {value:23,name:"Annaba"},
+    {value:24,name:"Guelma"},
+    {value:25,name:"Constantine"},
+    {value:26,name:"Médéa"},
+    {value:27,name:"Mostaganem"},
+    {value:28,name:"M'Sila"},
+    {value:29,name:"Mascara"},
+    {value:30,name:"Ouargla"},
+    {value:31,name:"Oran"},
+    {value:32,name:"El Bayadh"},
+    {value:33,name:"Illizi"},
+    {value:34,name:"Bordj Bou Arréridj"},
+    {value:35,name:"Boumerdès"},
+    {value:36,name:"El Tarf"},
+    {value:37,name:"Tindouf"},
+    {value:38,name:"Tissemsilt"},
+    {value:39,name:"El Oued"},
+    {value:40,name:"Khenchela"},
+    {value:41,name:"Souk Ahras"},
+    {value:42,name:"Tipaza"},
+    {value:43,name:"Mila"},
+    {value:44,name:"Aïn Defla"},
+    {value:45,name:"Naâma"},
+    {value:46,name:"Aïn Témouchent"},
+    {value:47,name:"Ghardaïa"},
+    {value:48,name:"Relizane"},
+    {value:49,name:"El M'ghair"},
+    {value:50,name:"El Menia"},
+    {value:51,name:"Ouled Djellal"},
+    {value:52,name:"Bordj Baji Mokhtar"},
+    {value:53,name:"Béni Abbès"},
+    {value:54,name:"Timimoun"},
+    {value:55,name:"Touggourt"},
+    {value:56,name:"Djanet"},
+    {value:57,name:"In Salah"},
+    {value:58,name:"In Guezzam"}
+];
+
     async function fetchData() {
       try {
         const response = await axios.get('https://localhost:7002/api/v1/Centrale');
@@ -38,8 +100,9 @@ const Ctrl:React.FC<props> = (props) => {
     }
     useEffect(()=>{
       fetchData();
-      console.log(props.userRole);
  },[])
+
+
   useEffect(() => {
       if(inputValue!=undefined)
  { 
@@ -57,6 +120,67 @@ const Ctrl:React.FC<props> = (props) => {
     else  fetchData();
   }, [inputValue])
  
+//   useEffect(() => {
+//     console.log("filter : ",Filtring)
+//     if(ChosenFiltrage!=undefined && Filtring!=0)
+// { 
+//   if(ChosenFiltrage=="Wilaya"){
+//     const filterCards = centraleData.filter((card:any) => {
+//       if (card && card.wilayaId) {
+//         setCurrentPage(1);
+//         return card.wilayaId ===Filtring
+//       } else {
+//         return false;
+//       }
+    
+//   });  
+// setFiltedCentrales(filterCards)
+//   } }
+//   else if(ChosenFiltrage=="Type"){
+//     const filterCards = centraleData.filter((card:any) => {
+//       if (card && card.nature) {
+//         setCurrentPage(1);
+//         return card.nature.includes(Filtring);
+//       } else {
+//         return false;
+//       }
+    
+//   });  
+// setFiltedCentrales(filterCards)
+//   }
+//   else  fetchData();
+// }, [Filtring,ChosenFiltrage])
+
+useEffect(() => {
+  const filterCentrales = () => {
+    let filterCards = centraleData;
+    if(Filtring==0){
+      setChosenFiltrage(undefined)
+      setFiltring(undefined)
+    }
+    if (ChosenFiltrage === "Wilaya" && Filtring !== 0) {
+      filterCards = centraleData.filter((card) => Number(card.wilayaId) === Number(Filtring));
+    } else if (ChosenFiltrage === "Type" && Filtring !== "All") {
+      filterCards = centraleData.filter((card) => card.nature.includes(Filtring));
+    }
+    
+    setFiltedCentrales(filterCards);
+  };
+
+  setCurrentPage(1);
+  
+  if (Filtring && ChosenFiltrage) {
+    filterCentrales();
+  } else {
+    setFiltedCentrales(centraleData);
+  }
+}, [Filtring, ChosenFiltrage]);
+
+
+
+
+
+
     const [currentPage, setCurrentPage] = useState(1);
     const postsPerPage= 4;
     const lastPostIndex = currentPage * postsPerPage;
@@ -70,6 +194,7 @@ const Ctrl:React.FC<props> = (props) => {
         isChecked: false
       }))
     );
+    
     useEffect(() => {
     if(FiltedCentrales)  {  const newCheckboxes = FiltedCentrales.map((lign: any) => ({
           id:lign.code,
@@ -126,18 +251,49 @@ const Ctrl:React.FC<props> = (props) => {
         onChange={(e) => setInputValue(e.target.value)} />
             <AiOutlineSearch  className="absolute top-[25%] left-[4%] text-[#E4EAF0] " ></AiOutlineSearch>
             </div>
-            <div  className="relative w-[85px] h-[32px] cursor-pointer flex justify-between items-center p-2 border border-solid border-[#E4EAF0] text-[#6E7886] "
-            onClick={()=>setfirstFilter(!firstFilter)}
-            >
+
+
+
+    
+     
+
+      
+
+
+            <div  className="relative min-w-[85px] h-[32px] cursor-pointer flex justify-between items-center p-2 border border-solid border-[#E4EAF0] text-[#6E7886] "
+            onClick={()=>setfirstFilter(!firstFilter)}   >
             Filter
           {  firstFilter ? <MdOutlineKeyboardArrowDown></MdOutlineKeyboardArrowDown> : <MdOutlineKeyboardArrowUp></MdOutlineKeyboardArrowUp>  }
+        { !firstFilter && <div className="w-[85px] py-2 flex flex-col gap-2 items-center absolute bottom-[-108px] border right-[50%] translate-x-[50%] bg-white ">
+            <p  className="border-solid border-[#E4EAF0] w-[85px] cursor-pointer px-2 hover:bg-[#03101e35] hover:text-black "  onClick={()=>setChosenFiltrage(undefined)}  >None</p>
+             <p className="border-solid border-[#E4EAF0] w-[85px] cursor-pointer px-2 hover:bg-[#03101e35] hover:text-black "  onClick={()=>setChosenFiltrage("Wilaya")} >Wilaya</p>
+             <p className="border-solid border-[#E4EAF0] w-[85px] cursor-pointer px-2 hover:bg-[#03101e35] hover:text-black "  onClick={()=>setChosenFiltrage("Type")} >Type</p>
+          </div>}
             </div>
-            <div  className=" relative w-[85px] h-[32px] cursor-pointer flex justify-between items-center p-2 border border-solid border-[#E4EAF0] text-[#6E7886]"
-              onClick={()=>setsecondFilter(!secondFilter)}
-            >
-            Filter
-            {  secondFilter ? <MdOutlineKeyboardArrowDown></MdOutlineKeyboardArrowDown> : <MdOutlineKeyboardArrowUp></MdOutlineKeyboardArrowUp>  }
-            </div>
+     {    ChosenFiltrage &&   <div   className=" relative text-[#6E7886]"  >
+            {  secondFilter ? <MdOutlineKeyboardArrowDown   className="absolute top-[50%] translate-y-[-50%] right-[5%] z-20" ></MdOutlineKeyboardArrowDown> : 
+            <MdOutlineKeyboardArrowUp  className="absolute z-20 top-[50%] translate-y-[-50%] right-[5%] " ></MdOutlineKeyboardArrowUp>  }
+{        ChosenFiltrage=="Wilaya"  &&  <select
+    onChange={(e) => setFiltring(e.target.value)} 
+    onClick={()=>setsecondFilter(!secondFilter)}
+    className=" appearance-none  relative w-[160px] h-[32px] cursor-pointer px-2 border border-solid border-[#E4EAF0] z-10 focus:outline-none focus:shadow-outline"  >
+      {Wilayas.map(wilaya=>{
+        return  <option value={wilaya.value}> {wilaya.name} </option>
+      })}
+            </select>}
+
+        { ChosenFiltrage=="Type" &&    <select
+        onChange={(e) => setFiltring(e.target.value)} 
+    onClick={()=>setsecondFilter(!secondFilter)}
+    className=" appearance-none  relative w-[90px] h-[32px] cursor-pointer px-2 border border-solid border-[#E4EAF0] z-10 focus:outline-none focus:shadow-outline"  >
+       <option value={0}>All</option>
+            <option value="TG">TG</option>
+          <option value="TV">TV</option>
+          <option value="CC">CC</option>
+            <option value="PV">PV</option>
+            <option value="Eolienne">Eolienne</option>
+            </select>}
+            </div>}
             <p className="text-[rgba(191,195,201,1)] " > Affichage de { FiltedCentrales ? FiltedCentrales.length  : "0" } résultats </p>
         </div>
    {( props.userRole=="Admin"  && FiltedCentrales ) &&  <button className="border border-solid border-[#E91010] font-semibold flex items-center justify-center gap-2 text-[#E91010] rounded-[10px] text-sm w-[110px] h-[35px] "   
